250ccb4e72df9601b0409aedfc503b055b945c54
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2023-10-27 23:23:12 +0200

    Revert "Update robots.ts"
    This reverts commit 20da090cea5db42d33f74eb0384ca019fade01cc.

--- app/robots.ts
+++ app/robots.ts
@@ -1,23 +1,14 @@
 import { MetadataRoute } from 'next';
 import { headers } from 'next/headers';
-
 export default function robots(): MetadataRoute.Robots {
   const headersList = headers();
   const host = headersList.get('host');
 
-  const AIBots = [
-    'GPTBot',
-    'ChatGPT-User',
-    'OmgiliBot',
-    'CCbot',
-    'FacebookBot',
-  ];
-
   return {
-    rules: AIBots.map((ai) => ({
-      userAgent: ai,
-      disallow: '/',
-    })),
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: `https://${host}/sitemap.xml`,
   };
 }
