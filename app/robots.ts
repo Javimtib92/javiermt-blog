20da090cea5db42d33f74eb0384ca019fade01cc
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2023-10-27 23:16:09 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,23 @@
 import { MetadataRoute } from 'next';
 import { headers } from 'next/headers';
+
 export default function robots(): MetadataRoute.Robots {
   const headersList = headers();
   const host = headersList.get('host');
 
+  const AIBots = [
+    'GPTBot',
+    'ChatGPT-User',
+    'OmgiliBot',
+    'CCbot',
+    'FacebookBot',
+  ];
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: AIBots.map((ai) => ({
+      userAgent: ai,
+      disallow: '/',
+    })),
     sitemap: `https://${host}/sitemap.xml`,
   };
 }
