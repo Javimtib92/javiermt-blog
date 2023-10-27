73dd94a85a0bb3ef954c57a8de17c3dc2c384a1a
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2023-10-27 11:08:22 +0200

    add automatic metadata to article page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,15 +1,13 @@
+import type { Metadata, ResolvingMetadata } from 'next';
 import { MarkdownPage } from '@/components/markdown-page';
 import { getAllArticles } from '@/utils/get-all-articles';
+import { getArticleData } from '@/utils/get-article-data';
 
-export async function generateStaticParams() {
-  return getAllArticles();
-}
-
-export default async function ArticlePage({
-  params,
-}: {
+type ArticlePageProps = {
   params: { slug: string };
-}) {
+};
+
+export default async function ArticlePage({ params }: ArticlePageProps) {
   const { slug } = params;
 
   return (
@@ -17,4 +15,27 @@ export default async function ArticlePage({
       <MarkdownPage slug={slug} />
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  return getAllArticles();
+}
+
+export async function generateMetadata(
+  { params }: ArticlePageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+
+  const { frontMatter } = await getArticleData(slug);
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: frontMatter.title,
+    description: frontMatter.shortDescription,
+    openGraph: {
+      images: [frontMatter.thumbnail, ...previousImages],
+    },
+  };
 }
