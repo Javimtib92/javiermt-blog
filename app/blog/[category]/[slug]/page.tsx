5076efcceea407b5387a1bac970f1357a516cb42
Javier Muñoz Tous <javimtib92@gmail.com>
2024-01-04 18:30:33 +0100

    try ppr for view count

--- app/blog/[category]/[slug]/page.tsx
+++ app/blog/[category]/[slug]/page.tsx
@@ -16,10 +16,6 @@ type ArticlePageProps = {
 const updateCounter = async (slug: string) => {
   'use server';
 
-  if (process.env.NODE_ENV !== 'production') {
-    return;
-  }
-
   try {
     await prisma.views.upsert({
       where: {
