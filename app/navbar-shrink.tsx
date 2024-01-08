36c0f12f22527a522d64efb57798c1573d955753
Javier Muñoz Tous <javimtib92@gmail.com>
2024-01-08 17:22:36 +0100

    add shrink navbar on scroll behavior

---
+++ app/navbar-shrink.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+
+export function NavbarShrink({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    if (!CSS.supports('animation-timeline: scroll()')) {
+      const navbar = document.querySelector('.navbar') as HTMLElement;
+      const threshold = 200; // Adjust this threshold as needed
+
+      window.addEventListener('scroll', handleScroll);
+
+      function handleScroll() {
+        if (!navbar) {
+          return;
+        }
+
+        const scrollY = window.scrollY || window.pageYOffset;
+
+        if (scrollY > threshold) {
+          navbar.style.setProperty('--navbar-shrink', '1');
+        } else {
+          navbar.style.setProperty('--navbar-shrink', '0');
+        }
+      }
+    }
+  }, []);
+
+  return children;
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,18 +2,22 @@ import { ThemeProvider } from '@/components/theme-provider';
 import NavigationLinks from './navigation-links';
 import { SiteLogo } from './site-logo';
 import PreferredThemeSwitch from '@/components/preferred-theme-switch';
+import { NavbarShrink } from './navbar-shrink';
 
 export function Navbar() {
   return (
-    <nav className='sticky top-0 mb-12 mt-4 flex flex-row items-start py-4 pl-2 pr-4 backdrop-blur md:-mx-[28px] md:px-5'>
-      <div className='flex min-w-full flex-row justify-between space-x-0'>
+    <nav className='navbar sticky top-0 mb-12 mt-4 flex flex-row items-start py-4 pl-2 pr-4 md:-mx-[28px] md:px-5'>
+      <div className='backdrop absolute top-0 h-20 min-w-full backdrop-blur md:-mx-[28px]'></div>
+      <div className='img-container z-10 flex min-w-full flex-row justify-between space-x-0'>
         <SiteLogo />
 
         <div className='flex gap-6'>
           <NavigationLinks />
-          <ThemeProvider>
-            <PreferredThemeSwitch></PreferredThemeSwitch>
-          </ThemeProvider>
+          <NavbarShrink>
+            <ThemeProvider>
+              <PreferredThemeSwitch></PreferredThemeSwitch>
+            </ThemeProvider>
+          </NavbarShrink>
         </div>
       </div>
     </nav>
