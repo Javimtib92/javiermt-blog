4929cd81a5c966667b30e1b5172a21121157f5ea
Javier Muñoz Tous <javimtib92@gmail.com>
2024-01-08 17:29:57 +0100

    fix mobile positioning

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,7 +7,7 @@ import { NavbarShrink } from './navbar-shrink';
 export function Navbar() {
   return (
     <nav className='navbar sticky top-0 mb-12 mt-4 flex flex-row items-start py-4 pl-2 pr-4 md:-mx-[28px] md:px-5'>
-      <div className='backdrop absolute top-0 h-20 min-w-full backdrop-blur md:-mx-[28px]'></div>
+      <div className='backdrop absolute left-[-8px] right-[-8px] top-0 h-20 min-w-full backdrop-blur md:-mx-[28px]'></div>
       <div className='img-container z-10 flex min-w-full flex-row justify-between space-x-0'>
         <SiteLogo />
 
