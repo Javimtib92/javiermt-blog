57fbe27c2cc8555e1290fdcc6979704741fa2e81
Javier Muñoz Tous <javimtib92@gmail.com>
2024-01-20 16:52:34 +0100

    fix sitemap urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,5 @@
 import { getAllArticles } from '@/utils/get-all-articles';
+import { getAllCategories } from '@/utils/get-all-categories';
 import { MetadataRoute } from 'next';
 import { headers } from 'next/headers';
 
@@ -8,6 +9,8 @@ export default function Sitemap(): MetadataRoute.Sitemap {
 
   const articles = getAllArticles();
 
+  const categories = getAllCategories();
+
   return [
     {
       url: 'https://' + host + '/',
@@ -15,11 +18,23 @@ export default function Sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly' as const,
       priority: 1,
     },
+    {
+      url: 'https://' + host + '/blog',
+      lastModified: '2018-10-20T01:46:40.000Z',
+      changeFrequency: 'monthly' as const,
+      priority: 0.7,
+    },
+    ...categories.map((category) => ({
+      url: 'https://' + host + '/blog/' + category,
+      lastModified: '2018-10-20T01:46:40.000Z',
+      changeFrequency: 'monthly' as const,
+      priority: 0.6,
+    })),
     ...articles.map(({ path, modifiedAt }) => ({
       url: 'https://' + host + path,
       lastModified: modifiedAt,
       changeFrequency: 'monthly' as const,
-      priority: 1,
+      priority: 0.6,
     })),
   ];
 }
